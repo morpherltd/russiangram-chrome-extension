87e3994fe18129f26b4af567ff2e5403ce1c3ffe
Master Weber <master.weber@outlook.com>
2022-01-28 08:58:37 +0300

    Added config for declinations tables

--- js/config.js
+++ js/config.js
@@ -5,6 +5,10 @@ App.config = {
         url: 'https://russiangram.com/api/GetPopUpInfo.aspx',
         timeout: 60 * 1000,
     },
+    declensionTable: {
+        url: 'https://dev5.morpher.ru/lemmy',
+        timeout: 60 * 1000,
+    },
     parsePhrase: {
         left: 100,
         right: 100,
