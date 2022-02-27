500f93c015af9134d643db082d40f8c8672553ea
Sergey Slepov <slepov@gmail.com>
2022-02-27 16:15:54 +0000

    New context menu item 'Add stress marks to this page'

--- js/context-menu.js
+++ js/context-menu.js
@@ -1,7 +1,13 @@
 chrome.contextMenus.create({
     'id': 'lookUp',
-    'title': 'Look up in RussianGram',
+    'title': 'Look up in RussianGram (Alt-DblClick)',
     'contexts': ['selection'],
+});
+
+chrome.contextMenus.create({
+    'id': 'stress-mark-page',
+    'title': 'Add stress marks to this page',
+    'contexts': ['page'],
 });
 
 chrome.contextMenus.onClicked.addListener(function(clickData) {
@@ -12,6 +18,16 @@ chrome.contextMenus.onClicked.addListener(function(clickData) {
         }, function(tabs) {
             chrome.tabs.sendMessage(tabs[0].id, {
                 action: 'lookUp',
+            });
+        });
+    }
+    if (clickData.menuItemId === 'stress-mark-page') {
+        chrome.tabs.query({
+            active: true,
+            currentWindow: true,
+        }, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {
+                msg: 'get_content',
             });
         });
     }
